c1d4df1bd7f3aeff8f320c68b25a889e3493d162
CYY99 <63221949+CYY99@users.noreply.github.com>
2021-09-14 17:17:03 -0700

    Initial commit 1 for testing

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def selfIntroduction():
+    print("I would like to lay a solid base on python and improve my programming skills. ")
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    selfIntroduction()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
