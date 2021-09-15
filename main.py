8408d6bfd582b41aadfeafa45fad6f49a10bf17b
CYY99 <63221949+CYY99@users.noreply.github.com>
2021-09-14 17:46:47 -0700

    test 3

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# This is my main.py
+# Purpose:
 
 
+# description: This is function that prints some sentences
+# Arguments: no argument
+# return type: no return value
 def selfIntroduction():
     print("I would like to lay a solid base on python and improve my programming skills. ")
-
 
 
 # Press the green button in the gutter to run the script.
